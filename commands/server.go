a07b9a0acb3038f74bb538bb3b5f4e5c33160818
wildlifehexagon <crossthebreeze@gmail.com>
2019-10-11 16:05:53 -0500

    feat: add handling for service worker and manifest.json

--- commands/server.go
+++ commands/server.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"compress/gzip"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -12,6 +13,26 @@ import (
 	"github.com/gorilla/handlers"
 	"gopkg.in/urfave/cli.v1"
 )
+
+func sendSW(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("service-worker.js")
+	if err != nil {
+		http.Error(w, "Couldn't read service worker file", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/javascript; charset=utf-8")
+	w.Write(data)
+}
+
+func sendManifest(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("manifest.json")
+	if err != nil {
+		http.Error(w, "Couldn't read manifest.json", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write(data)
+}
 
 func ServeAction(c *cli.Context) error {
 	// create log folder
@@ -33,6 +54,8 @@ func ServeAction(c *cli.Context) error {
 		vhandler = http.StripPrefix(prefixPath, fs)
 	}
 	http.Handle(subURL, lmw.Middleware(vhandler))
+	http.HandleFunc("/service-worker.js", sendSW)
+	http.HandleFunc("/manifest.json", sendManifest)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("serving client url %s", r.URL.Path)
 		http.ServeFile(w, r, filepath.Join(c.String("folder"), "index.html"))
